5a469f99a2119963b4fe068aee9a5cd3c443da7e
daianefernandes <daianedsdaia@gmail.com>
2022-05-15 17:40:41 -0300

    ajustando atalhos da home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,44 @@
+
 import styled from "styled-components";
+import { Button } from "../common-components/Button/Button";
 import { Card } from "../common-components/Card/Card";
 import { Heading } from "../common-components/Heading/Heading";
-import { Subtitle } from "../common-components/Subtitle/Subtitle";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
 `;
 
+const HomeButton = styled(Button)`
+  margin-top: 15px;
+`;
+
 export function Home() {
+
+  let navigateAbout = useNavigate(); 
+  const routeAbout = () =>{ 
+    let path = `/sobre`; 
+    navigateAbout(path);
+  }
+
+  let navigateFormation = useNavigate(); 
+  const routeFormation = () =>{ 
+    let path = `/formacao`; 
+    navigateFormation(path);
+  }
+
+  let navigateExperience = useNavigate(); 
+  const routeExperience = () =>{ 
+    let path = `/experiencia`; 
+    navigateExperience(path);
+  }
+
+  let navigateHobbie = useNavigate(); 
+  const routeHobbie = () =>{ 
+    let path = `/hobbies`; 
+    navigateHobbie(path);
+  }
+
   return (
     <>
       <Container>
@@ -15,10 +46,10 @@ export function Home() {
       </Container>
 
       <Card>
-        <Subtitle>SOBRE VOCÊ</Subtitle>
-        <Subtitle>FORMAÇÃO</Subtitle>
-        <Subtitle>EXPERIÊNCIA</Subtitle>
-        <Subtitle>HOBBIES</Subtitle>
+        <HomeButton onClick={routeAbout}>SOBRE VOCÊ</HomeButton>
+        <HomeButton onClick={routeFormation}>FORMAÇÃO</HomeButton>
+        <HomeButton onClick={routeExperience}>EXPERIÊNCIA</HomeButton>
+        <HomeButton onClick={routeHobbie}>HOBBIES</HomeButton>
       </Card>
     </>
   );
